1f7bd26918cbe3657fd772ec87abad7257bbd4ad
renz vanni bato <renzvanni.dev@gmail.com>
2023-09-05 23:45:41 -0700

    static crew page

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -3,7 +3,36 @@ import React from "react";
 function Crew() {
   return (
     <div className="crew">
-      <h1>Crew</h1>
+      <p className="pick">
+        <span>01</span> PICK YOUR DESTINATION
+      </p>
+
+      <div className="information-container">
+        <div className="container-1">
+          <div className="image-container">
+            <img
+              src="../../public/assets/crew/image-douglas-hurley.png"
+              alt=""
+            />
+          </div>
+
+          <ul>
+            <li>
+              <div></div>
+            </li>
+          </ul>
+        </div>
+
+        <div className="container-2">
+          <h4>COMMANDER</h4>
+          <h3>DOUGLAS HURLEY</h3>
+          <p>
+            Douglas Gerald Hurley is an American engineer, former Marine Corps
+            pilot and former NASA astronaut. He launched into space for the
+            third time as commander of Crew Dragon Demo-2.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
