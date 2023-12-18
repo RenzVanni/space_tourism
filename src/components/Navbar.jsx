4a617bc00c9befb5f0928c838f629d9818cf2c5c
renz vanni bato <renzvanni.dev@gmail.com>
2023-12-18 15:06:36 +0800

    head img

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="logo-container">
-        <img src="/public/assets/shared/logo.svg" alt="logo" />
+        <img src="/assets/shared/logo.svg" alt="logo" />
       </div>
       <div className="nav">
         <NavLink to="/">Home</NavLink>
@@ -12,10 +12,7 @@ function Navbar() {
         <NavLink to="technology">Technology</NavLink>
       </div>
       <div className="hamburger-container">
-        <img
-          src="/public/assets/shared/icon-hamburger.svg"
-          alt="hamburger-icon"
-        />
+        <img src="/assets/shared/icon-hamburger.svg" alt="hamburger-icon" />
       </div>
     </div>
   );
